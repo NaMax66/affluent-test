51a9b2149297c6735f3445feb279e83dae53d399
NaMax66 <max.naid@mail.ru>
2021-09-27 12:06:57 +0500

    post visualisation done

---
+++ src/components/posts/PostListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="post-list-item">
+    <h5>{{ post.title }}</h5>
+    <p>{{ post.body }}</p>
+  </li>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue';
+import { Post } from '@/specification/DTO/Post';
+
+export default {
+  name: 'PostListItem',
+  props: {
+    post: {
+      type: Object as PropType<Post>,
+      required: true
+    }
+  },
+};
+</script>
+
+<style scoped>
+.post-list-item {
+  padding-bottom: 0.3rem;
+  max-width: 600px;
+}
+</style>
--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="page-posts">
     <h1>Posts page</h1>
+    <!-- enhancement: add post list filtration by userId -->
     <div>
-      <ul>
-        <li v-for="post in postList" :key="post.id">
-          {{ post.id }}
-        </li>
+      <ul class="post-list">
+        <post-list-item class="post-list-item" v-for="post in postList" :post="post" :key="post.id" />
       </ul>
     </div>
   </div>
@@ -15,10 +14,11 @@
 import { defineComponent } from 'vue';
 import { mapActions, mapGetters } from 'vuex';
 import { Post } from '@/specification/DTO/Post';
+import PostListItem from '@/components/posts/PostListItem.vue';
 
 export default defineComponent({
   name: 'PostsPage',
-
+  components: { PostListItem },
   data: () => ({
     isListLoading: false
   }),
@@ -55,5 +55,15 @@ export default defineComponent({
   width: 100%;
   max-width: var(--width-max-desktop);
   margin: 0 auto;
+}
+
+.post-list {
+  margin-top: 2rem;
+  list-style: none;
+}
+
+.post-list-item:not(:last-child) {
+  margin-bottom: 1rem;
+  border-bottom: 1px solid var(--c-light-gray);
 }
 </style>
